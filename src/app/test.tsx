540ffcdd7a10be67229e93e3a0fb87f9ac40388a
Dennis Martin Cual <denniscual0716@gmail.com>
2025-04-12 17:53:12 +0800

    init support for ProfilerTimeline

--- src/app/test.tsx
+++ src/app/test.tsx
@@ -1,12 +1,15 @@
 "use client";
-import React from "react";
+import React, { useEffect, useLayoutEffect, useSyncExternalStore } from "react";
 import { simulateDelay } from "./util";
 import {
   Profiler,
   ProfilerControls,
+  profilerDataStore,
   ProfilerGraph,
   ProfilerProvider,
+  useProfilerProvider,
 } from "./profiler";
+import Timeline from "./timeline";
 
 export default function TestList() {
   return (
@@ -25,6 +28,7 @@ export default function TestList() {
             <h2 className="text-lg font-bold mb-6">Profiler</h2>
             <div className="space-y-5">
               <ProfilerControls />
+              <ProfilerTimeline />
               <ProfilerGraph />
             </div>
           </div>
@@ -38,14 +42,29 @@ function TestComponent() {
   const [text, setText] = React.useState("");
   const deferredText = React.useDeferredValue(text);
 
-  simulateDelay(3);
+  simulateDelay(10);
+
+  useLayoutEffect(() => {
+    simulateDelay(25);
+  }, [text]);
 
   return (
     <div>
+      <Profiler id="Foo">
+        <Foo />
+      </Profiler>
       <h2 className="text-lg font-bold mb-4">Input and List</h2>
       <input
         value={text}
-        onChange={(e) => setText(e.target.value)}
+        onChange={(e) => {
+          setText(e.target.value);
+
+          performance.mark("Start Keyup");
+          // const now = Date.now();
+          // while (Date.now() - now < 100) {}
+          performance.mark("End Keyup");
+          performance.measure("Keyup", "Start Keyup", "End Keyup");
+        }}
         className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
         placeholder="Type to trigger re-rendering..."
       />
@@ -57,6 +76,7 @@ function TestComponent() {
 }
 
 const List = React.memo(function List({ text }: { text: string }) {
+  simulateDelay(10);
   const items: React.ReactNode[] = [];
   for (let i = 0; i < 5; i++) {
     items.push(
@@ -93,3 +113,92 @@ function SlowItem({ text }: { text: string }) {
     </li>
   );
 }
+
+// TODO:
+// - create RendersTimeline using our timeline codes. Check the React Profiler Timeline
+// for reference.
+// - create UserEventsTimeline.
+// - add replayer/rrweb
+// - profiling doesn't work in prod (build and then run pnpm start).
+function ProfilerTimeline() {
+  const data = useSyncExternalStore(
+    profilerDataStore.subscribe,
+    profilerDataStore.getSnapshot,
+    profilerDataStore.getSnapshot
+  );
+
+  const [inputEvents, setInputEvents] = React.useState<any>([]);
+  const { isProfilingStarted } = useProfilerProvider();
+
+  const renderEvents: any[] = [];
+  data.renders.forEach((render) => {
+    // if (render.actualDuration === 0) return;
+
+    const event = {
+      type: render.id,
+      category: "Render",
+      y: 3,
+      start: render.startTime,
+      end: render.startTime + render.actualDuration,
+      color: "#9c27b0",
+    };
+    renderEvents.push(event);
+  });
+
+  useEffect(() => {
+    const observer = new PerformanceObserver((list) => {
+      list.getEntries().forEach((entry) => {
+        if (!["input", "click"].includes(entry.name)) {
+          return;
+        }
+
+        const event = {
+          type: entry.name,
+          category: "Input",
+          y: 2,
+          start: entry.startTime,
+          end: entry.startTime + entry.duration,
+          color: "#4dabf5",
+        };
+        setInputEvents((prev) => [...prev, event]);
+      });
+    });
+
+    // Register the observer for events
+    observer.observe({
+      type: "event",
+      buffered: true,
+      // @ts-expect-error expect error
+      durationThreshold: 0,
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  if (isProfilingStarted || data.profiles.length === 0) return null;
+
+  console.log({ inputEvents });
+
+  return (
+    <div>
+      <Timeline renders={renderEvents} inputs={inputEvents} />{" "}
+    </div>
+  );
+}
+
+const Foo = React.memo(function Foo() {
+  const [toggleFoo, setToggleFoo] = React.useState(false);
+  simulateDelay(200);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setToggleFoo(!toggleFoo);
+        }}
+      >
+        Update Foo Slow Component
+      </button>
+    </div>
+  );
+});
