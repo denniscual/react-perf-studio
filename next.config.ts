51b2babd3377fbda89143c96cc2893b4334e8bdc
Dennis Martin Cual <denniscual0716@gmail.com>
2025-09-20 21:10:52 +0800

    disable typescript errsors in build prod

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // We temporarily set this into `true` due to issue in `react-scan-dist`.
+    //
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
